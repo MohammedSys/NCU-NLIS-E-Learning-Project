d3769c9bcf4561f522844e4b4d558b8038efb80a
hippoandy <hippo1993928@gmail.com>
2018-03-01 13:42:38 -0800

    Progress bar content.

---
+++ objects/pgbar-content.php
@@ -0,0 +1,8 @@
+<!-- Main Content -->
+<div id="content margin-t-60">
+	<div class="p-box">
+		<div class="loader">Loading...</div>
+	</div>
+	<br><br><br>
+	<div id="info" class="pgbar-msg"></div>
+</div>
