441181a848b2d691025ba7772f4bcf03b8f0194d
hippoandy <hippo1993928@gmail.com>
2018-03-01 13:43:58 -0800

    Updated the SQL command and the condition for account match checking.

--- php/accountConfirm.php
+++ php/accountConfirm.php
@@ -1,24 +1,25 @@
 <?php session_start(); ?>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <?php
-	include("connection.php");
+	include( "connection.php" );
 	$username = $_POST['account'];
 	$password = $_POST['password'];
 
 	if( $username == null && $password == null ) errorLogin();
 
-	$sql ="SELECT * FROM userAccount where name = '$username' and password = PASSWORD( '$password' )";
+	$sql ="SELECT * FROM userAccount where username = '$username' and password = PASSWORD( '$password' )";
 	$result = mysql_query($sql);
 
-	if( $row = @mysql_fetch_row($result) )
+	$num = mysql_num_rows( $result );
+	if( $num != 0 )
 	{
+		$row = @mysql_fetch_row($result);
 		// if the user is manager, open the manager system
-		if ( $row[ 2 ] ) echo '<meta http-equiv=REFRESH CONTENT=3;url=../manager/material-Select.php>';
+		if ( $row[ 1 ] == 1 ) echo '<meta http-equiv=REFRESH CONTENT=3;url=../manager/material-Select.php>';
 		// otherwise, open the ordinary system
 		else echo '<meta http-equiv=REFRESH CONTENT=3;url=../main.php>';
 		$_SESSION['account'] = $username;
 	}
-	// else errorLogin();
+	else errorLogin();
 
 	function errorLogin()
 	{
