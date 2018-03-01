186f1f0ee4955bb10e5db5bafae81583eb1975ef
hippoandy <hippo1993928@gmail.com>
2018-03-01 02:15:22 -0800

    The most general section of <head> (no any path specific).

---
+++ objects/head-meta.php
@@ -0,0 +1,9 @@
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="Author" content="NCU IM Group 16">
+<meta name="Description" content="This is a learning system to help the members of New Life Co. learn how to write webpages.">
+<meta name="Creation-Date" content="01-Sep-2015 08:00">
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>E-learning System</title>
+
