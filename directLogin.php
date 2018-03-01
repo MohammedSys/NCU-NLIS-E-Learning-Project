3aeb1cbabe56140650e4e635dcfcee4e7de49a7d
hippoandy <hippo1993928@gmail.com>
2018-03-01 13:39:46 -0800

    Merged the common component with php/mailer.php and php/accountConfirm.php.

--- directLogin.php
+++ directLogin.php
@@ -1,42 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-		<meta name="Author" content="NCU IM Group 16">
-		<meta name="Description" content="This is a learning system to help the members of New Life Co. learn how to write webpages.">
-		<meta name="Creation-Date" content="01-Sep-2015 08:00">
-		
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<meta http-equiv="REFRESH" content="3;url=index.php">
-		<title>E-learning System</title>
-		
-		<!--icon-->
-		<link href="assets/img/favicon.ico" rel="SHORTCUT ICON">
-		
-		<!--Bootstrap-->
-		<link rel="stylesheet" href="../assets/css/bootstrap.min.css">
-		<link rel="stylesheet" href="../assets/css/bootstrap-social.css">
-		<link rel="stylesheet" href="../assets/css/font-awesome.css">
-		
-		<!--Customized CSS Settings-->
-		<link rel="stylesheet" href="../assets/css/hippo.css">
-		<link rel="stylesheet" href="../assets/css/progress-bar.css">
-		<link rel="stylesheet" href="../assets/css/directLogin.css">
-		<style>
-			
-		</style>
-	</head>
-	
-	<body class="full">
-		
-		<!-- Main Content -->
-		<div id="content margin-t-60">		
-			<div id="info">
-				<h1><span class="label label-warning">您尚未登入!</span></h1>
-				<p>將自動轉址至登入頁面</p>
-			</div>
-			<div class="loader">Loading...</div>
-	
-		</div>
-	</body>
+<head>
+<?php include 'objects/head.php'; ?>
+<script>
+$( document ).ready( function()
+{
+	document.getElementById( "info" ).innerHTML = '<h1><span class="label label-warning">您尚未登入!</span></h1> \
+		<br><p>將自動轉址至登入頁面</p>';
+	document.getElementById( "info" ).style.display = "block";
+});
+</script>
+</head>
+<body class="full">
+<?php include 'objects/pgbar-content.php'; ?>
+</body>
 </html>
