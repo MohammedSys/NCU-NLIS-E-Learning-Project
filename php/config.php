4aa92f72a525f263b7b0aa91aa7019af9514552c
hippoandy <hippo1993928@gmail.com>
2018-02-28 23:49:10 -0800

    The general parameters are stored here.

---
+++ php/config.php
@@ -0,0 +1,23 @@
+<?php
+
+// for the database connection
+$servername = "127.0.0.1";
+$username = "";
+$password = "";
+$db_name = "";
+
+
+// for the mailer.php
+$mail_subject = "E-learning 忘記密碼";
+
+$mailing_server = 'ssl://smtp.gmail.com';
+$mailing_port = 465;
+
+// who send the mail?
+$mail_name = "";
+// mailing account
+$mail_acc = "";
+// passwod for mailing account
+$mail_pwd = "";
+
+?>
