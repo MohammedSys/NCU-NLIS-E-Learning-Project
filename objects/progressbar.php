e92183d64f8e439f6d85b2afae39f337e494e32f
hippoandy <hippo1993928@gmail.com>
2018-03-01 13:42:12 -0800

    General progress bar html. Part of the code is not located in objects/pgbar-content.php.

--- objects/progressbar.php
+++ objects/progressbar.php
@@ -4,25 +4,9 @@
 <html lang="en">
 <head>
 <?php include '../objects/head-meta.php'; ?>
-
-<!--icon-->
-<link href="../assets/img/newlife_circle.png" rel="SHORTCUT ICON">	
-
-<!--Bootstrap-->
-<link rel="stylesheet" href="../assets/css/bootstrap.min.css">
-<link rel="stylesheet" href="../assets/css/bootstrap-social.css">
-<link rel="stylesheet" href="../assets/css/font-awesome.css">
-
-<!--Customized CSS Settings-->
-<link rel="stylesheet" href="../assets/css/hippo.css">
-<link rel="stylesheet" href="../assets/css/progress-bar.css">
+<?php include '../objects/head-link-sub.php'; ?>
 </head>
 <body class="full">
-<!-- Main Content -->
-<div id="content margin-t-60">
-	<div class="p-box">
-		<div class="loader">Loading...</div>
-	</div>
-</div>
+<?php include '../objects/pgbar-content.php'; ?>
 </body>
 </html>
