b21897b6bd9ef2c8fd66b50f2b4856be0bfd359b
hippoandy <hippo1993928@gmail.com>
2018-02-28 23:45:57 -0800

    The common header section for all the page.

---
+++ objects/footer.php
@@ -0,0 +1,27 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                <ul class="list-inline text-center">
+                    <li><a class="transparent-btn" href="http://vivialife.com/TW/" target="_blank">好日子購物網</a>
+                    </li>
+                    <li><a class="transparent-btn" href="http://www.sci.org.tw/" target="_blank">脊髓新樂園</a>
+                    </li>
+                    <li><a class="transparent-btn" href="http://www.sci.org.tw/" target="_blank">脊髓損傷潛能發展中心</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="row row-margin">
+            <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                <ul class="list-inline text-center">
+                    <li><a class="btn btn-social-icon btn-facebook" href="https://www.facebook.com/nlishsinchu?hc_location=stream" target="_blank">
+                            <i class="fa fa-facebook"></i>
+                        </a>
+                    </li>
+                </ul>
+                <p class="copyright">Copyright &copy; ISQ Lab. NCU 2015</p>
+            </div>
+        </div>
+    </div>
+</footer>
