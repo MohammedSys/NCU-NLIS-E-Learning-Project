f9e87cc5e782f3807b64e22e4cd9029e6704e285
hippoandy <hippo1993928@gmail.com>
2018-03-01 13:46:09 -0800

    New navbar general component.

---
+++ objects/navbar-left-list.php
@@ -0,0 +1,17 @@
+<!-- Brand and toggle get grouped for better mobile display -->
+<div class="navbar-header">
+    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+    </button>
+    <div id="logo_printer">
+        <table class="a-hover txt-white" border="0" cellspacing="0" cellpadding="0">
+            <tr onclick="openlink('./index.php')">
+                <td><img alt="Brand" class="brand-icon" src="assets/img/newlife_circle.png"></td>
+                <td width="130px"><div class="systemname">E-learning System<br/>新生命資訊服務 Co.</div></td>
+            </tr>
+        </table>
+    </div>
+</div>
