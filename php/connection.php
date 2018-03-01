b29b29071fb21d89e005bb7ae87de374a346ce91
hippoandy <hippo1993928@gmail.com>
2018-02-28 23:53:27 -0800

    Updated code format.

--- php/connection.php
+++ php/connection.php
@@ -1,14 +1,12 @@
 <?php
     include( "./config.php" );
 
-    // Create connection
-    $conn = mysql_connect($servername,$username,$password);
+    // create connection
+    $conn = mysql_connect( $servername, $username, $password );
 
-    // Check connection
-    if ($conn->connect_error) {
+    // check connection
+    if ($conn->connect_error)
         die("Connection failed: " . $conn->connect_error);
-    }
 
-    if(!@mysql_select_db($db_name))
-        die("connect fail"); 
+    if(!@mysql_select_db($db_name)) die("connect fail");
 ?>
