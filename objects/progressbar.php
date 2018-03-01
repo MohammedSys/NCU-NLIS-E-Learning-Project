32ac465dbb2a4a84208dc7ee827001c5879b3dac
hippoandy <hippo1993928@gmail.com>
2018-03-01 02:16:31 -0800

    This is the general progress bar component currently used by php/accountConfirm.php and php/mailer.php.

---
+++ objects/progressbar.php
@@ -0,0 +1,28 @@
+<!-- used by mailer.php and accountConfirm.php -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<?php include '../objects/head-meta.php'; ?>
+
+<!--icon-->
+<link href="../assets/img/newlife_circle.png" rel="SHORTCUT ICON">	
+
+<!--Bootstrap-->
+<link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+<link rel="stylesheet" href="../assets/css/bootstrap-social.css">
+<link rel="stylesheet" href="../assets/css/font-awesome.css">
+
+<!--Customized CSS Settings-->
+<link rel="stylesheet" href="../assets/css/hippo.css">
+<link rel="stylesheet" href="../assets/css/progress-bar.css">
+</head>
+<body class="full">
+<!-- Main Content -->
+<div id="content margin-t-60">
+	<div class="p-box">
+		<div class="loader">Loading...</div>
+	</div>
+</div>
+</body>
+</html>
