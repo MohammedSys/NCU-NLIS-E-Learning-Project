c09f35b7c770b0121e24c6ffca0d2a2cad647cf2
hippoandy <hippo1993928@gmail.com>
2018-03-01 22:27:06 -0800

    Update menu toggle method using getElementByID instead of jQuery.

--- assets/js/menu-toggled.js
+++ assets/js/menu-toggled.js
@@ -1,7 +1,12 @@
-$(document).ready(function () {
-	$('#left-menu-toggle').on('click', function ()
-	{
-		// $('#B').toggleClass('push-right');
-		$('#side-menu-main').toggleClass('set-menu-open');
-	});
-});
+// $(document).ready(function () {
+// 	$('#left-menu-toggle').on('click', function ()
+// 	{
+// 		// $('#B').toggleClass('push-right');
+// 		$('#side-menu-main').toggleClass('set-menu-open');
+// 	});
+// });
+
+function openSideMenu( id, classname )
+{
+	$('#' + id).toggleClass( classname );
+}
