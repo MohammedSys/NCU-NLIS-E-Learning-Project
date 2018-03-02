125f0407d304bcca1410d167981989c7cc53d5cc
hippoandy <hippo1993928@gmail.com>
2018-03-01 22:28:31 -0800

    Updated.

--- php/config.php
+++ php/config.php
@@ -1,21 +1,20 @@
 <?php
 
 // for the database connection
-$servername = "127.0.0.1";
+$servername = "127.0.0.1:3306";
 $username = "";
 $password = "";
 $db_name = "";
 
 
 // for the mailer.php
-$mail_subject = "E-learning 忘記密碼";
+$mail_subject = "E-learning System 忘記密碼";
 
-// $mailing_server = 'ssl://smtp.gmail.com';
 $mailing_server = 'smtp.gmail.com';
 $mailing_port = 465;
 
 // who send the mail?
-$mail_name = "";
+$mail_name = "E-learning System Support";
 // mailing account
 $mail_acc = "";
 // passwod for mailing account
