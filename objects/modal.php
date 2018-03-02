8f5131ee465c707fe81ddba9f4d466d7c632a017
hippoandy <hippo1993928@gmail.com>
2018-03-01 22:31:49 -0800

    The general modal dialogue view.

---
+++ objects/modal.php
@@ -0,0 +1,18 @@
+<!-- Modal Message -->
+<div class="modal fade" id="Modal">
+    <div class="modal-dialog">
+    <div class="modal-content">
+        <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title">系統訊息</h4>
+        </div>
+        <div class="modal-body">
+        <!-- <p>發現系統錯誤:</p> -->
+        <p id="modalMessage"></p>
+        </div>
+        <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">確定</button>
+        </div>
+    </div>
+    </div>
+</div>
