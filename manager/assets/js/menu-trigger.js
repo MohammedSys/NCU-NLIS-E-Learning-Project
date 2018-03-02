995402383792d700f4ab8ad9c620c4db5e8b832b
hippoandy <hippo1993928@gmail.com>
2018-03-01 22:28:13 -0800

    Deleted unnecessary files.

--- manager/assets/js/menu-trigger.js
+++
@@ -1,5 +0,0 @@
-$("#menu-toggle").click(function(e) {
-	// e.preventDefault();
-	// $("#wrapper").toggleClass("toggled");
-	$('#sidebar-menu-main').toggleClass('set-menu-open');
-});
--- manager/assets/js/resize-iframe.js
+++
@@ -1,5 +0,0 @@
-function resizeIframe(obj)
-{
-	obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
-	$('#iframe-parent').css("height", obj.style.height);
-}
