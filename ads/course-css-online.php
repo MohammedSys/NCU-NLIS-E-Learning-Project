9280ad02d13ec4cc7a5660d59cfa0651a875ceab
hippoandy <hippo1993928@gmail.com>
2018-03-01 19:21:36 -0800

    The slides for main.php now stores here and will be loaded by php scirpt.

---
+++ ads/course-css-online.php
@@ -0,0 +1,10 @@
+<div class="item active">			
+    <div class="bannerImage">
+        <a href="#"><img src="assets/img/mslide01.png" alt=""></a>
+    </div>
+                
+    <div class="caption row-fluid">
+        <div class="span4"><h3>新課程上線！<br /><br />CSS3 基礎課程</h3></div>                	
+        <!--<div class="span8"><p>層疊樣式表（英語：Cascading Style Sheets，簡寫CSS），又稱串樣式清單、級聯樣式表、串接樣式表、層疊樣式表、階層式樣式表，一種用來為結構化文件（如HTML文件或XML應用）添加樣式（字型、間距和顏色等）的電腦語言。本課程希望藉由程式碼與實際執行結果，讓同學能更加容易上手！</p></div>-->
+    </div>
+</div><!-- /Slide1 -->
---
+++ ads/course-js-ongoing.php
@@ -0,0 +1,14 @@
+<div class="item">
+					
+    <div align="center" class="bannerImage">
+        <a href="#"><img src="assets/img/mslide02.png" alt=""></a>
+    </div>
+                
+    <div class="caption row-fluid">
+        <div class="span4"><h3>新課程預告：<br /><br />JavaScript 基礎課程</h3>
+            <!--<a class="btn btn-mini" href="#">&raquo; Read More</a>-->
+        </div>                	
+        <!--<div class="span8"><p><br />JavaScript，一種直譯式程式語言，是一種動態型別、基於原型的語言，內建支援類別。它的直譯器被稱為 JavaScript引擎，為瀏覽器的一部分，廣泛用於用戶端的腳本語言，最早是在HTML網頁上使用，用來給 HTML網頁增加動態功能。在設計動態網頁時，必定會使用 JS，這也使得 JS 同樣為撰寫網頁必備的技能之一。</p></div>-->
+    </div>
+                                            
+</div><!-- /Slide2 -->
