cd447304f6be49c37827a3e701aa4be0819b5b08
hippoandy <hippo1993928@gmail.com>
2018-03-01 22:29:42 -0800

    Updated view and head links.

--- manager/material-Upload.php
+++ manager/material-Upload.php
@@ -1,131 +1,86 @@
 <?php session_start(); ?>
-<?php 
-	require 'php/permission.php';
-?>
+<?php require 'php/permission.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta charset="utf-8">
-		<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame
-		Remove this if you use the .htaccess -->
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-		<meta name="Author" content="NCU ISQ Group 16">
-		<meta name="Description" content="The manager interface of New Life Co. E-learning System.">
-		<meta name="Creation-Date" content="01-Sep-2015 08:00">		
-		<meta name="viewport" content="width=device-width, initial-scale=1">	
-		<title>E-learning System</title>	
-		<!--icon-->
-		<link href="../assets/img/newlife_circle.png" rel="SHORTCUT ICON">
-		<!--Bootstrap-->
-		<link rel="stylesheet" href="../assets/css/bootstrap.min.css">
-		<link rel="stylesheet" href="../assets/css/bootstrap-social.css">
-		<link rel="stylesheet" href="../assets/css/font-awesome.css">
-		
-		<!-- fixed menu setting -->
-		<link href="../assets/css/hippo.css" rel="stylesheet">
-		<link href="assets/css/simple-sidebar.css" rel="stylesheet">
-		<!--Customized CSS Settings-->
-		<link href="assets/css/material-upload.css" rel="stylesheet">
-		
-		<!--JavaScript Library-->
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-		<script type="text/javascript" src="../assets/js/bootstrap.js"></script>
-		<!--<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>	-->
-		<!--Customized JacaScript-->
-		<script type="text/javascript" src="assets/js/manager-submitCheck.js"></script>
-	</head>
-
-	<body>
-		<div id="wrapper" class="toggled"><!-- Sidebar -->
-	        <?php include 'manager-menu.php'; ?>
-	        <?php include 'manager-header.php'; ?>
-			<div class="fixed">
-				<div class="page-header">
-					<h2 class="text-primary">教材上傳 <small>請依欄位輸入相關資料</small></h2>
-				</div>
-				<div>
-					<form id="materialForm" class="form-horizontal" method="post" enctype="multipart/form-data" action="php/material-Upload-process.php">
-						<div class="form-group">
-							<label for="subjectInput" class="col-md-2 control-label">教材主題名稱</label>
-							<div class="col-md-10">
-								<input id="subjectInput" class="form-control" type="text" name="subject" required />
-								<span class="help-block">請輸入教材主題。例如：CSS</span>
-							</div>
-						</div>
-						<div class="form-group">
-							<label for="chapterInput" class="col-md-2 control-label">教材章節</label>
-							<div class="col-md-10">
-								<input id="chapterInput" class="form-control" min="0" type="number" name="chapter" required />
-								<span class="help-block">請輸入教材章節，請輸入純數字。</span>
-							</div>
-						</div>
-						<div class="form-group">
-							<label for="titleInput" class="col-md-2 control-label">教材標題</label>
-							<div class="col-md-10">
-								<input id="titleInput" class="form-control" type="text" name="title" required />
-								<span class="help-block">請輸入教材標題，無須包含章節與主題。</span>
-							</div>
-						</div>
-						<div class="form-group">
-							<label for="introInput" class="col-md-2 control-label">教材簡介</label>
-							<div class="col-md-10">
-								<input id="introInput" class="form-control" type="text" name="intro" required />
-								<span class="help-block">請輸入簡短的教材簡介，將用於首頁的教材介紹。</span>
-							</div>
-						</div>
-						<div class="form-group">
-							<label for="descriptInput" class="col-md-2 control-label">教材敘述</label>
-							<div class="col-md-10">
-								<textarea id="descriptInput" class="form-control m-descirption" form="materialForm" name="descript" required ></textarea>
-								<span class="help-block">請輸入教材敘述，敘述這個章節大致的學習內容，將用於搜尋頁面。</span>
-							</div>
-						</div>
-						<div class="form-group">
-							<label for="teacherInput" class="col-md-2 control-label">教材講師</label>
-							<div class="col-md-10">
-								<input id="teacherInput" class="form-control" type="text" name="teacher" required />
-								<span class="help-block">請輸入講師名稱，用來區別同主題不同講師的教材。</span>
-							</div>
-						</div>
-						<div class="form-group">
-							<label for="fileInput" class="col-md-2 control-label">教材檔案</label>
-							<div class="col-md-10">
-								<input id="fileInput" type="file" name="material_file" required />
-								<span class="help-block">請選擇上傳檔案，必須為 .pdf 格式檔案。</span>
-							</div>
-						</div>
-						<div>
-							<button class="btn btn-primary" type="button" onclick="material_UploadCheck()">上傳</button>
-							<button class="btn btn-danger" type="button" onclick="history.back()" >取消上傳</button>
-						</div>
-					</form>
-				</div>
+<head>
+<?php include '../objects/head-meta.php'; ?>
+<?php include '../objects/head-link-sub.php'; ?>
+<!--Customized CSS Settings-->
+<link href="assets/css/material-upload.css" rel="stylesheet">
+<link href="assets/css/simple-sidebar.css" rel="stylesheet">
+<!--Customized JacaScript-->
+<script type="text/javascript" src="assets/js/manager-submitCheck.js"></script>
+</head>
+<body>
+	<div id="wrapper" class="toggled"><!-- Sidebar -->
+		<?php include 'manager-menu.php'; ?>
+		<?php include 'manager-header.php'; ?>
+		<div class="fixed">
+			<div class="page-header">
+				<h2 class="text-primary">教材上傳 <small>請依欄位輸入相關資料</small></h2>
 			</div>
-			
-			<!-- Modal Message -->
-			<div class="modal fade" id="Modal">
-			  <div class="modal-dialog">
-			    <div class="modal-content">
-			      <div class="modal-header">
-			        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-			        <h4 class="modal-title">系統訊息</h4>
-			      </div>
-			      <div class="modal-body">
-			        <p>發現系統錯誤:</p>
-			        <p id="modalMessage">系</p>
-			      </div>
-			      <div class="modal-footer">
-			        <button type="button" class="btn btn-default" data-dismiss="modal">確定</button>
-			      </div>
-			    </div><!-- /.modal-content -->
-			  </div><!-- /.modal-dialog -->
-			</div><!-- /.modal -->
-			
-			
-		</div><!-- /#sidebar-wrapper -->
-		
-		<!-- Menu Toggle Script -->
-		<script type="text/javascript" src="assets/js/menu-trigger.js"></script>
-	</body>
+			<div>
+				<form id="materialForm" class="form-horizontal" method="post" enctype="multipart/form-data" action="php/material-Upload-process.php">
+					<div class="form-group">
+						<label for="subjectInput" class="col-md-2 control-label">教材主題名稱</label>
+						<div class="col-md-10">
+							<input id="subjectInput" class="form-control" type="text" name="subject" required />
+							<span class="help-block">請輸入教材主題。例如：CSS</span>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="chapterInput" class="col-md-2 control-label">教材章節</label>
+						<div class="col-md-10">
+							<input id="chapterInput" class="form-control" min="0" type="number" name="chapter" required />
+							<span class="help-block">請輸入教材章節，請輸入純數字。</span>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="titleInput" class="col-md-2 control-label">教材標題</label>
+						<div class="col-md-10">
+							<input id="titleInput" class="form-control" type="text" name="title" required />
+							<span class="help-block">請輸入教材標題，無須包含章節與主題。</span>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="introInput" class="col-md-2 control-label">教材簡介</label>
+						<div class="col-md-10">
+							<input id="introInput" class="form-control" type="text" name="intro" required />
+							<span class="help-block">請輸入簡短的教材簡介，將用於首頁的教材介紹。</span>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="descriptInput" class="col-md-2 control-label">教材敘述</label>
+						<div class="col-md-10">
+							<textarea id="descriptInput" class="form-control m-descirption" form="materialForm" name="descript" required ></textarea>
+							<span class="help-block">請輸入教材敘述，敘述這個章節大致的學習內容，將用於搜尋頁面。</span>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="teacherInput" class="col-md-2 control-label">教材講師</label>
+						<div class="col-md-10">
+							<input id="teacherInput" class="form-control" type="text" name="teacher" required />
+							<span class="help-block">請輸入講師名稱，用來區別同主題不同講師的教材。</span>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="fileInput" class="col-md-2 control-label">教材檔案</label>
+						<div class="col-md-10">
+							<input id="fileInput" type="file" name="material_file" required />
+							<span class="help-block">請選擇上傳檔案，必須為 .pdf 格式檔案。</span>
+						</div>
+					</div>
+					<div>
+						<button class="btn btn-primary" type="button" onclick="material_UploadCheck()">上傳</button>
+						<button class="btn btn-danger" type="button" onclick="javascript:location.href='material-Select.php" >取消上傳</button>
+					</div>
+				</form>
+			</div>
+		</div>
+
+		<!-- Modal Message -->
+		<?php include '../objects/modal.php'; ?>
+
+	</div><!-- /#sidebar-wrapper -->
+</body>
 </html>
--- manager/material-View.php
+++ manager/material-View.php
@@ -1,86 +1,58 @@
 <?php session_start(); ?>
 <?php 
 	require 'php/permission.php';
-	include("php/connection.php");
+	include( "php/connection.php" );
 	//設定編碼，避免中文字出現亂碼
-	mysql_query("set names 'utf8'");
+	mysql_query( "set names 'utf8'" );
 ?>
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta charset="utf-8">
-		<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame
-		Remove this if you use the .htaccess -->
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-		<meta name="Author" content="NCU ISQ Group 16">
-		<meta name="Description" content="The manager interface of New Life Co. E-learning System.">
-		<meta name="Creation-Date" content="01-Sep-2015 08:00">		
-		<meta name="viewport" content="width=device-width, initial-scale=1">	
-		<title>E-learning System</title>	
-		<!--icon-->
-		<link href="../assets/img/newlife_circle.png" rel="SHORTCUT ICON">
-		<!--Bootstrap-->
-		<link rel="stylesheet" href="../assets/css/bootstrap.min.css">
-		<link rel="stylesheet" href="../assets/css/bootstrap-social.css">
-		<link rel="stylesheet" href="../assets/css/font-awesome.css">	
-		<!--Customized CSS Settings-->
-		<link rel="stylesheet" href="assets/css/material-View.css">
-		<!--JavaScript Library-->
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-		<script type="text/javascript" src="../assets/js/bootstrap.js"></script>
-		<!--<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>	-->
-	</head>
+<head>
+<?php include '../objects/head-meta.php'; ?>
+<?php include '../objects/head-link-sub.php'; ?>
+<!--Customized CSS Settings-->
+<link rel="stylesheet" href="assets/css/material-View.css">
+</head>
+<body id="manager-mv">
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<h4 class="text-primary">教材瀏覽</h4>
+		</div>
+		<div class="panel-body">
+			<form id="materialForm" method="post" action="">
+				<div class="table-responsive">
+					<table class="table table-hover table-bordered table-condensed">
+						<tr><th>No</th><th>主題</th><th>章節</th><th>標題</th><th>簡介</th><th>敘述</th><th>講師</th></tr>
+						<?php
+							//如果接收到教材主題按鈕指令，更新session
+							if ($_POST['subject'] != null) {
+								$_SESSION['materialSubject'] = $_POST['subject'];								
+							}
+							//用session儲存教材sql指令變數
+							$subject = $_SESSION['materialSubject'];
+							$sql = "SELECT * FROM teachingMaterial WHERE subject = '$subject' ORDER BY CAST(chapter AS UNSIGNED)";
+							$result = mysql_query($sql);
+							while ( $row = mysql_fetch_row($result) )
+							{
+								echo "<tr><td><input type=\"checkbox\" name=\"chapter[]\" value=\"".$row[2]."\" /></td>";
+								echo "<td>".$row[1]."</td><td>".$row[2]."</td><td>".$row[3]."</td>";
+								echo "<td>".$row[4]."</td><td>".$row[5]."</td><td>".$row[6]."</td></tr>";
+							}		    				
+						?>
+					</table>			    		
+				</div>
+				<div id="submitDiv">
+					<input type="hidden" name="subject" value="<?php echo $subject; ?>" />
+					<button class="btn btn-primary" type="button" onclick="changeAction('update')">修改教材資訊</button>
+					<button class="btn btn-danger" type="button" onclick="changeAction('del')">刪除教材章節</button>
+				</div>
+			</form>
+			<script type="text/javascript" src="assets/js/material-View_Button.js"></script>
+		</div> <!--end of panel body-->
+	</div> <!--end of panel-->
 
-	<body id="manager-mv">
-		<div class="panel panel-default">
-			<div class="panel-heading">
-		    	<h4 class="text-primary">教材瀏覽</h4>
-		    </div>
-		    <div class="panel-body">
-		    	<form id="materialForm" method="post" action="">
-			    	<div class="table-responsive">
-			    		<table class="table table-hover table-bordered table-condensed">
-			    			<th>
-			    				<td>主題</td>
-			    				<td>章節</td>
-			    				<td>標題</td>
-			    				<td>簡介</td>
-			    				<td>敘述</td>
-			    				<td>講師</td>
-			    			</th>
-			    			<?php
-			    				//如果接收到教材主題按鈕指令，更新session
-			    				if ($_POST['subject'] != null) {
-									$_SESSION['materialSubject'] = $_POST['subject'];								
-								}
-								//用session儲存教材sql指令變數
-								$subject = $_SESSION['materialSubject'];
-								$sql = "SELECT * FROM teachingMaterial WHERE subject = '$subject' ORDER BY CAST(chapter AS UNSIGNED)";
-								$result = mysql_query($sql);
-								while ( $row = mysql_fetch_row($result) ) {
-									echo "<tr>";
-									echo "<td><input type=\"checkbox\" name=\"chapter[]\" value=\"".$row[2]."\" /></td>";
-									echo "<td>".$row[1]."</td>";
-									echo "<td>".$row[2]."</td>";
-									echo "<td>".$row[3]."</td>";
-									echo "<td>".$row[4]."</td>";
-									echo "<td>".$row[5]."</td>";
-									echo "<td>".$row[6]."</td>";
-									echo "</tr>";
-									
-								}		    				
-			    			?>
-			    		</table>			    		
-			    	</div>
-			    	<div id="submitDiv">
-		    			<input type="hidden" name="subject" value="<?php echo $subject; ?>" />
-		    			<button class="btn btn-primary" type="button" onclick="changeAction('update')">修改教材資訊</button>
-		    			<button class="btn btn-danger" type="button" onclick="changeAction('del')">刪除教材章節</button>
-		    		</div>
-		    	</form>
-		    	<script type="text/javascript" src="assets/js/material-View_Button.js"></script>
-		    </div> <!--end of panel body-->
-		</div><!--end of panel-->
-	</body>
+	<!-- Modal Message -->
+	<?php include '../objects/modal.php'; ?>
+
+</body>
 </html>
