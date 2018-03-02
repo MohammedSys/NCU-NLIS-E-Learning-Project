3ff777d6bcc00ed150de3c644dc06fe8de93e112
hippoandy <hippo1993928@gmail.com>
2018-03-01 19:19:49 -0800

    Renamed files.

---
+++ objects/insystem-navbar-left.php
@@ -0,0 +1,18 @@
+<!-- Brand and toggle get grouped for better mobile display -->
+<div class="navbar-header">
+    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+    </button>
+    <div id="logo_printer">
+        <table class="a-hover txt-white" border="0" cellspacing="0" cellpadding="0">
+            <tr id="left-menu-toggle">
+                <a href="#left-menu"></a>
+                <td><img alt="Brand" class="brand-icon" src="assets/img/newlife_circle.png"></td>
+                <td width="40px"><div class="systemname"><span class="glyphicon glyphicon-menu-hamburger"></span></div></td>
+            </tr>
+        </table>
+    </div>
+</div>
